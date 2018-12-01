e5d9b2f5016fde9b967ea0745f2094ec9a298169
Alexander Ramirez <kuroneko247@gmail.com>
2018-11-30 21:56:19 -0800

    Added lighthouse, text counter, and modified waypoint system

---
+++ 13Towers/Assets/counter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class counter : MonoBehaviour {
+
+    
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
